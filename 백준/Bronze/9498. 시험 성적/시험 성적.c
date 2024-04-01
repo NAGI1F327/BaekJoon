6154eb20e7d6e46b3face7a059ccca080d1fdd71
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-04-02 08:20:48 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int score = 0;
+	scanf("%d", &score);
+
+	if (90 <= score && score <= 100)
+		printf("A \n", score);
+	else if (80 <= score && score <= 89)
+		printf("B \n", score);
+	else if (70 <= score && score <= 79)
+		printf("C \n", score);
+	else if (60 <= score && score <= 69)
+		printf("D \n", score);
+	else
+		printf("F \n", score);
+}
