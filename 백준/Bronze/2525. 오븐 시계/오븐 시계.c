7db5683b55d9a82bae0858ada3fd63eddb228653
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-04-03 12:46:04 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int H, M, m;
+
+	scanf("%d %d %d", &H, &M, &m);
+
+	H += m / 60;
+	M += m % 60;
+
+	if (M >= 60) {
+		++H;
+		M -= 60;
+	}
+	if (H >= 24) {
+		H -= 24;
+	}
+	printf("%d %d", H, M);
+}
