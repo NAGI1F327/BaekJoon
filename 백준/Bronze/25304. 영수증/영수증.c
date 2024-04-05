cfadf70a2b2a535096dbbd850e3361a23198749d
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-04-05 10:52:48 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int X, N, a, b;
+	int i = 0;
+    
+	scanf("%d \n %d", &X, &N);
+
+	for (i = 0; i < N; i++) {
+		scanf("%d %d", &a, &b);
+		X -= a * b;
+	}
+	if (X == 0)
+		printf("Yes");
+	else
+		printf("No");
+
+	return 0;
+}
