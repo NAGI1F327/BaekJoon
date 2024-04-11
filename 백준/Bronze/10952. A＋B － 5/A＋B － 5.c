509f36191aa389b56b24030355944096b1b94d7c
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-04-11 11:59:33 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -3,12 +3,11 @@
 int main() {
 	int A, B;
 
-	while (1) {   // 무한 while 루프
+	while (1) {   
 		scanf("%d %d", &A, &B);
-		if (A, B == 0)   // A, B의 값이 0이면 while 루프 종료
-			break;   // 루프 바깥으로 빠져나감
+		if (A, B == 0)   
+			break; 
 		printf("%d \n", A + B);
-		
 	}
 	return 0;
 }
