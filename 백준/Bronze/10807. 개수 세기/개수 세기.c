0a5176b69f4fcf165784bdd228aff5501a6cc55a
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-04-30 18:35:38 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.c
+++ 백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main(){
-    int n, array[100], v;
+    int N, array[100], v;
     int cnt = 0;
     
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
         scanf("%d",&array[i]);
     }
 
     scanf("%d", &v);
-    for (int j = 0; j < n; j++) {
+    for (int j = 0; j < N; j++) {
         if(array[j] == v)
             cnt++;
     }
