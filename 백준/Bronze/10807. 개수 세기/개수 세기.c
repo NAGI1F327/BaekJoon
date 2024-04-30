b54d761d5bb2bb571e2a81581bc42f29803f5802
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-04-30 19:29:01 +0900

    개수 세기.c

--- 백준/Bronze/10807. 개수 세기/개수 세기.c
+++ 백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -4,11 +4,13 @@ int main(){
     int cnt = 0;
     
     scanf("%d", &N);
+    
     for (int i = 0; i < N; i++) {
         scanf("%d",&array[i]);
     }
 
     scanf("%d", &v);
+    
     for (int j = 0; j < N; j++) {
         if(array[j] == v)
             cnt++;
