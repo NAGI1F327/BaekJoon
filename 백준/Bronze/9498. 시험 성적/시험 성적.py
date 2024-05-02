ef1a4ef3849a477de2c1fd6d8c5513727e96f7a1
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-05-02 21:08:01 +0900

    [Bronze V] Title: 시험 성적, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+score = int(input())
+
+if (90 <= score and score <= 100):
+    print("A \n")
+elif (80 <= score and score <= 89):
+    print("B \n")
+elif (70 <= score and score <= 79):
+    print("C \n")
+elif (60 <= score and score <= 69):
+    print("D \n")
+else:
+    print("F \n")
