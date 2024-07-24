de8309280f87cdfcd18f3eb5c44a6fdfc442079b
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-07-24 18:24:49 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,7 +1,7 @@
 A, B = input().split()
 
-reversed_A = int(A[2] + A[1] + A[0])
-reversed_B = int(B[2] + B[1] + B[0])
+reversed_A = int(A[::-1]) 
+reversed_B = int(B[::-1])
 
 if reversed_A > reversed_B:
     print(reversed_A)
