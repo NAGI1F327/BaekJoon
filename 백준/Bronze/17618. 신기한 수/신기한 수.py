b8f1670ede00cf68f0dbcef2faad4fa404357a5f
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-07-29 11:31:53 +0900

    [Bronze III] Title: 신기한 수, Time: 3120 ms, Memory: 112464 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17618. 신기한 수/신기한 수.py
+++ 백준/Bronze/17618. 신기한 수/신기한 수.py
@@ -1,10 +1,12 @@
+def sum_of_digits(n):
+    return sum(int(digit) for digit in str(n))
+
 N = int(input().strip())
 
 count = 0
 for i in range(1, N + 1):
-    digit_sum = sum(int(digit) for digit in str(i))
-    
-    if digit_sum != 0 and i % digit_sum == 0:
+    digit_sum = sum_of_digits(i)
+    if digit_sum > 0 and i % digit_sum == 0:
         count += 1
 
 print(count)
