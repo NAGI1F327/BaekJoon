24303ca86c77e588cddf935c16d3622740950739
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-09-19 19:16:54 +0900

    1998년생인 내가 태국에서는 2541년생？！.java

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int year = sc.nextInt();
+		int y = sc.nextInt();
 		
-		System.out.println(year - 543);
+		System.out.println(y - 543);
 		
 		sc.close();
 	}
