d5b96144a2482da485564ff6ddbda30e5da2b1c4
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-09-20 19:46:29 +0900

    [Bronze IV] Title: 주사위 세개, Time: 168 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String args[]) {
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-    int c = sc.nextInt();
-    int max = 0;
-    
-	if (a == b && a == c) {
-		System.out.println(10000 + a * 1000);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int max = 0;
+		
+		if (a == b && a == c) {
+			System.out.println(10000 + a * 1000);
+		}
+		else if (a == b || a == c) {
+			System.out.println(1000 + a * 100);
+		}
+		else if (b == c) {
+			System.out.println(1000 + b * 100);
+		}
+		else {
+			max = Math.max(a, Math.max(b, c));
+			System.out.println(max * 100);
+		}
+		sc.close();
 	}
-    else if (a == b || a == c) {
-        System.out.println(1000 + a * 100);
-    }
-    else if (b == c) {
-        System.out.println(1000 + b * 100);
-    }
-    else {
-        max = Math.max(a, Math.max(b, c));
-        System.out.println(max * 100);
-    }
-    sc.close();
-  }
 }
