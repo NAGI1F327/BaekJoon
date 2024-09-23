1c27f4fe6d1ed5b588e0f6933e45a43e8777137e
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-09-23 20:24:17 +0900

    [Bronze IV] Title: 영수증, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.py
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -4,10 +4,10 @@ N = int(input())
 i = 0
 while i < N:
     a, b = map(int, input().split())
-    X = X - (a * b)
-    i = i + 1
+    X -= a * b
+    i += 1
 
-if (X == 0):
+if X == 0:
     print("Yes")
 else:
     print("No")
