0c9a8b85d4bebdef6ad7e3c3c839ba25bf956b7d
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-09-24 17:03:40 +0900

    [Bronze III] Title: 최소, 최대, Time: 160 ms, Memory: 4896 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.c
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -1,16 +1,22 @@
-#include<stdio.h>
-int main(){
-    int N, min = 1000000, max = -1000000;
+#include <stdio.h>
+
+int main() {
+    int i, N;
+	int min = 1000000, max = -1000000;
+	
     scanf("%d", &N);
     int array[N];
-    
-    for (int i = 0; i < N; i++) {
+	
+    for (i = 0; i < N; i++) {
         scanf("%d", &array[i]);
-        if(min > array[i])
+        if (min > array[i]) {
             min = array[i];
-        if(max < array[i])
+        }
+        if (max < array[i]) {
             max = array[i];
+        }
     }
+	
     printf("%d %d", min, max);
     return 0;
 }
