608d9620b354353169404e05f4c65a1a4b054660
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-09-24 17:08:05 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.c
+++ 백준/Bronze/2562. 최댓값/최댓값.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
+
 int main() {
-    int array[9] = {0,};
-    int max = 0;
-    int p = 0;
-    
+    int array[9] = {0};
+    int max = 0, p = 0;
+	
     for (int i = 0; i < 9; i++) {
-        scanf("%d", &array[i]);  
-        if(max < array[i]) {
+        scanf("%d", &array[i]);
+        if (max < array[i]) {
             max = array[i];
-            p = i+1;
+            p = i + 1;
         }
     }
+	
     printf("%d\n%d", max, p);
-    
     return 0;
 }
