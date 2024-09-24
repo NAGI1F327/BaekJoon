8b3debf495df9c1df9bf4c3853ad8f61de13352f
Nagi🌧 <165064097+NAGI1F327@users.noreply.github.com>
2024-09-24 17:09:15 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,4 +1,7 @@
 N_list = [int(input()) for _ in range(9)]
 
-print(max(N_list))
-print(N_list.index(max(N_list)) + 1)
+max_value = max(N_list)
+max_index = N_list.index(max_value) + 1
+
+print(max_value)
+print(max_index)
